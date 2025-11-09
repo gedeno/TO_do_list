a705882b696d1e16fb01cca78d7f75fa36da56fe
gedeno <gedenogeremew@gmail.com>
2025-11-09 20:04:41 +0300

    function is add

--- TO_do/views.py
+++ TO_do/views.py
@@ -1,4 +1,6 @@
 from  django.http import HttpResponse
 
 def taxi(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world this my first function .")
+def car(request):
+    return HttpResponse("Hello, world. this is my scond function.")
