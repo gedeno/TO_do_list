bb1f921e2a2328fba1889f5ebc1341d554f17150
gedeno <gedenogeremew@gmail.com>
2025-11-09 20:06:51 +0300

    function is add

--- TO_do/views.py
+++ TO_do/views.py
@@ -4,3 +4,5 @@ def taxi(request):
     return HttpResponse("Hello, world this my first function .")
 def car(request):
     return HttpResponse("Hello, world. this is my scond function.")
+def car(request):
+    return HttpResponse("Hello, world. this is my scond function.")
