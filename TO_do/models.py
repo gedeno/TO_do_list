707751ab16ae9a8f6b7dc0a894ebdafcae2eecec
gedeno <gedenogeremew@gmail.com>
2025-12-05 18:32:26 +0300

    new class add

--- TO_do/models.py
+++ TO_do/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class ppl(models.Model):
-    task = models.CharField(max_length=300)
-    cond = models.CharField(max_length =34) 
+class ToDolist(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+        
+class item(models.Model):
+    todolist = models.ForeignKey(ToDolist,on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+    complex = models.BooleanField()
+    def __str__(self):
+        return self.text
