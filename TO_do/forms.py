22d5974d817166321b0c1004d2f639d79170f871
gedeno <gedenogeremew@gmail.com>
2025-12-06 20:46:08 +0300

    small endition

--- TO_do/forms.py
+++ TO_do/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class CreateNewlist(forms.Form):
     name = forms.CharField(label="name",max_length=300)
-    cheak = forms.BooleanField()
+    cheak = forms.BooleanField(required= False)
